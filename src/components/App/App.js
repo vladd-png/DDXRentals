c7324b0ef1c6c81ef8baf0349a2c2a97219e36d9
Virginia Ladd <virginialadd2@gmail.com>
2020-02-05 00:24:47 -0700

    Create a second app class for design

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import Form from '../Form/Form.js';
-import Animation from '../Animation/Animation.js';
+// import Form from '../Form/Form.js';
+// import Animation from '../Animation/Animation.js';
 import './App.css';
+import Map from '../Map/Map.js';
 
 class App extends Component {
   constructor() {
@@ -12,8 +13,7 @@ class App extends Component {
   render () {
     return (
       <div className="app">
-        <Animation />
-        <Form />
+        <Map />
       </div>
     );
   }
