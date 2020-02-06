8fb169923abb3ee9180e6a9add1c5cfc6f0bc5f2
Virginia Ladd <virginialadd2@gmail.com>
2020-02-06 15:14:21 -0700

    Map component updates the parameter passed through to match app state

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -8,16 +8,16 @@ const Map = (props) => {
     <div className='map' name='cap-hill'>
       <img id='map-image' src={ map } alt='neon map of denver with neighborhoods outlined'/>
 
-      <div id='cap-hill' className='active-hood' onClick={() => props.updateNeighborhoodInfo('caphill')}></div>
-      <div onClick={() => props.updateNeighborhoodInfo('lohi')}>
+      <div id='cap-hill' className='active-hood' onMouseEnter={() => props.updateNeighborhoodInfo('Capitol Hill')}></div>
+      <div onMouseEnter={() => props.updateNeighborhoodInfo('Lower Highlands')}>
         <div className='active-hood' id='lohi-1' name='lohi'></div>
         <div id='lohi-2'></div>
       </div>
-      <div onClick={() => props.updateNeighborhoodInfo('parkhill')}>
+      <div onMouseEnter={() => props.updateNeighborhoodInfo('Park Hill')}>
         <div className='active-hood' name='park-hill' id='park-hill-1'></div>
         <div className='active-hood' name='park-hill' id='park-hill-2'></div>
       </div>
-      <div onClick={() => props.updateNeighborhoodInfo('rino')}>
+      <div onMouseEnter={() => props.updateNeighborhoodInfo('River North')}>
         <div className='active-hood' name='rino' id='rino-1'></div>
         <div id='rino-2'></div>
         <div id='rino-3'></div>
