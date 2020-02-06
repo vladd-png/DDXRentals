dc70b50a918b1a345402bdde2d5d9182c6501e6f
Virginia Ladd <virginialadd2@gmail.com>
2020-02-05 17:58:16 -0700

    Update app to hold neighborhood component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,6 +3,8 @@ import React, { Component } from 'react';
 // import Animation from '../Animation/Animation.js';
 import './App.css';
 import Map from '../Map/Map.js';
+import Nav from '../Nav/Nav.js';
+import Neighborhood from '../Neighborhood/Neighborhood.js';
 
 class App extends Component {
   constructor() {
@@ -13,7 +15,9 @@ class App extends Component {
   render () {
     return (
       <div className="app">
+        <Nav />
         <Map />
+        <Neighborhood />
       </div>
     );
   }
