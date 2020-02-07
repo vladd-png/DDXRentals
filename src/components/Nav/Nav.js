9c10351977c169fa84a3f7a6fe25a28c9f6f3837
Virginia Ladd <virginialadd2@gmail.com>
2020-02-06 20:41:03 -0700

    Add links to nava nd style

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -3,6 +3,8 @@ import avatar from '../../data/ski-avatar.png'
 import snowflake from '../../data/snowflake.png'
 import logo from '../../data/DDR-logo.png'
 import './Nav.scss';
+import { Link } from 'react-router-dom';
+
 
 class Nav extends Component {
   constructor() {
@@ -14,14 +16,14 @@ class Nav extends Component {
   render() {
     return (
       <nav className='nav-bar'>
-        <img className='logo-img' src={ logo } alt='DDR logo' />
+        <Link to={`/`}><img className='logo-img' src={ logo } alt='DDR logo' /></Link>
         <h3 className='welcome-msg'>Welcome interpolate Name! Thanks for choose DDR for your Radical Adventure</h3>
         <div className='user-display'>
           <h3 className='user-info'>Marcus Kleveland</h3>
           <h3 className='user-info'>SwedeshSkiTeam@sendit.com</h3>
         </div>
         <img className='avatar-img user-info' src={ avatar } alt='winter avatar with beanie and goggles' />
-        <h3 className='user-info'>Logout</h3>
+        <button type='button' className='user-info' id='logout-btn'>Logout</button>
         <img className='snowflake user-info' src={ snowflake } alt='snowflake favorite icon' />
       </nav>
     )
