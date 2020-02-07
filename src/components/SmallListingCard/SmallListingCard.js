c1c051ddd1bef8b7e7072154e2cc82ee03fc12d4
Zachary Nemeroff <zanemeroff@gmail.com>
2020-02-06 21:14:18 -0700

    Refactor props

--- src/components/SmallListingCard/SmallListingCard.js
+++ src/components/SmallListingCard/SmallListingCard.js
@@ -31,8 +31,8 @@ const SmallListingCard = (props) => {
           <div className='additional-images'>image</div>
         </div>
         <div className='additional-info'>
-          <h3>Beds: 2</h3>
-          <h3>Baths: 1.5</h3>
+          <h3>Beds: {props.beds}</h3>
+          <h3>Baths: {props.baths}</h3>
           <h3><img className='icon' src={featuresIcon} alt='location icon'/>features:</h3>
           <ul>
             {allFeatures}
