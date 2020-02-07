d0b696e46dffdc9ca75be04433abca0afbe7c7ab
Virginia Ladd <virginialadd2@gmail.com>
2020-02-06 20:41:37 -0700

    Create ListingContainer component

---
+++ src/components/ListingContainer/ListingContainer.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './ListingContainer.scss'
+import SmallListingCard from '../SmallListingCard/SmallListingCard.js';
+
+const ListingContainer = (props) => {
+  const allListings = props.listings.map(place => {
+    return <SmallListingCard
+      area={place.area_id}
+      name={place.name}
+      address={place.address.street}
+      addressZip={place.address.zip}
+      cost={place.details.cost_per_night}
+      features={place.details.features}
+    />
+  })
+  return (
+    <section className='listing-container'>
+      {allListings}
+    </section>
+  )
+}
+
+export default ListingContainer;
