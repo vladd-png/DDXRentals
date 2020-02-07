926dda537a200674643f38325ef1834125254aac
Zachary Nemeroff <zanemeroff@gmail.com>
2020-02-06 23:00:05 -0700

    Add initial tests

---
+++ src/components/Nav/Nav.test.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Nav from './Nav';
+import { shallow } from 'enzyme';
+
+describe('Nav', () => {
+
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<Nav />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  it('should be an instance of Nav component', () => {
+    const wrapper = shallow(<Nav
+
+      />);
+      expect(wrapper).toMatchSnapshot();
+  });
+
+});
