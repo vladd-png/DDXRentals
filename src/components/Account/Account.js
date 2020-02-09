84cbc89a0f99f7df756c8be546c755eecdef21ee
Virginia Ladd <virginialadd2@gmail.com>
2020-02-09 14:32:46 -0700

    style cards displayed on account

--- src/components/Account/Account.js
+++ src/components/Account/Account.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './Account.scss'
+import SmallListingCard from '../SmallListingCard/SmallListingCard.js';
 import one from '../../data/one.png';
 import two from '../../data/two.png';
 import three from '../../data/three.png';
@@ -8,6 +9,7 @@ import five from '../../data/fivr.png';
 import six from '../../data/six.png';
 import seven from '../../data/seven.png';
 let avatarImg;
+let favoritedListings;
 
 class Account extends Component {
   constructor(props) {
@@ -22,19 +24,34 @@ class Account extends Component {
     console.log(random);
     let characters = [one, two, three, four, five, six, seven];
     avatarImg = characters[random];
+    favoritedListings = this.props.favorites.map(place => {
+      return <SmallListingCard
+        id={place.id}
+        area={place.area}
+        name={place.name}
+        address={place.address}
+        addressZip={place.addressZip}
+        cost={place.cost}
+        features={place.features}
+        beds={place.beds}
+        baths={place.baths}
+        />
+    })
   }
   render() {
     return (
       <section id='account-page'>
-        <img src={ avatarImg } id='account-avatar' alt='avatar image from the ssx tricky game' />
-        <div id='account-display'>
-          <h1 id='account-name'>{this.state.account.name}</h1>
-          <h3 id='account-email'>{this.state.account.email}</h3>
-          <h3 id='account-value'>Your Custom {this.state.account.value} Ride Summary</h3>
-          <p>Welcome back to your Tubular Home Page. Here you can see all your Favorited Hip Hop Hot Spots. Funky Dope!</p>
+        <div id='account-img'>
+          <img src={ avatarImg } id='account-avatar' alt='avatar image from the ssx tricky game' />
+          <div id='account-display'>
+            <h1 id='account-name'>{this.state.account.name}</h1>
+            <h3 id='account-email'>{this.state.account.email}</h3>
+            <h3 id='account-value'>Your Custom {this.state.account.value} Ride Summary</h3>
+            <p>Welcome back to your Tubular Home Page. Here you can see all your Favorited Hip Hop Hot Spots. Funky Dope!</p>
+          </div>
         </div>
-        <div>
-          <h4>{this.state.account.value}</h4>
+        <div id='account-listing'>
+          <h4>{favoritedListings}</h4>
         </div>
       </section>
     )
