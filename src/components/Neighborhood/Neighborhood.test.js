cbf30923b62233cb207a09a04bb81cd13daafbe0
Zachary Nemeroff <zanemeroff@gmail.com>
2020-02-09 17:31:38 -0700

    Added Neighborhood render data

--- src/components/Neighborhood/Neighborhood.test.js
+++ src/components/Neighborhood/Neighborhood.test.js
@@ -7,13 +7,24 @@ describe('Neighborhood', () => {
 
   it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<Neighborhood />, div);
+    const mockListing = {
+
+    }
+    ReactDOM.render(<Neighborhood
+      areas={{chosenHood:
+        {
+          id: 0,
+          name: 'Select An Area on the Left to View Full Details',
+          location: null,
+          about: '',
+          listings: ["/api/v1/listings/3921"]
+        }
+      }}/>, div);
     ReactDOM.unmountComponentAtNode(div);
   });
 
   it('should be an instance of Neighborhood component', () => {
     const wrapper = shallow(<Neighborhood
-
       />);
       expect(wrapper).toMatchSnapshot();
   });
