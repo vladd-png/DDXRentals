ef0266a356d0a70613f47ae1aed7b1fef5d80afd
Virginia Ladd <virginialadd2@gmail.com>
2020-02-10 22:58:01 -0700

    Create a snowflake conditional

--- src/components/Animation/Animation.js
+++ src/components/Animation/Animation.js
@@ -2,9 +2,17 @@ import React from 'react';
 import './Animation.scss';
 import Snowflake from './Snowflake.js';
 
-const Animation = () => {
+const Animation = (props) => {
+  let snowflakeAmt = 150;
   let snowflake = [];
-  for (var i = 0; i < 150; i ++) {
+  if (props.amount.value === 'Business') {
+    snowflakeAmt = 100;
+  } else if(props.amount.value === 'Recreational') {
+    snowflakeAmt = 300;
+  } else if(props.amount.value === 'EXTREME') {
+    snowflakeAmt = 1000;
+  }
+  for (var i = 0; i < `${snowflakeAmt}`; i ++) {
     snowflake.push(Math.floor(Math.random() * 1000))
   }
   const flakes = snowflake.map(flake => <Snowflake locationX={flake - 50} locationY={Math.floor(Math.random() * 1500) - 100} key={Math.random()}/>);
