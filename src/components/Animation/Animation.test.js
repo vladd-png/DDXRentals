bac152cb103137e91a0c12f1dad104ffa2c4f885
Virginia Ladd <virginialadd2@gmail.com>
2020-02-10 17:17:02 -0700

    Implement mockRandom in testing

--- src/components/Animation/Animation.test.js
+++ src/components/Animation/Animation.test.js
@@ -2,20 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Animation from './Animation';
 import { shallow } from 'enzyme';
+import { mockRandom } from 'jest-mock-random';
 
 describe('Animation', () => {
 
-  it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<Animation />, div);
-    ReactDOM.unmountComponentAtNode(div);
-  });
+  beforeEach(() => {
+    mockRandom([0.1, 0.2, 0.3, 0.6]);
+ });
 
   it('should be an instance of Animation', () => {
-    const wrapper = shallow(<Animation
-
-      />);
-      expect(wrapper).toMatchSnapshot();
+    const wrapper = shallow(<Animation />);
+    expect(wrapper).toMatchSnapshot();
   });
 
 });
