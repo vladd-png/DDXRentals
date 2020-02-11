9abe757c0f1a32a57073f573148a6ee279c9d1aa
Virginia Ladd <virginialadd2@gmail.com>
2020-02-10 22:21:03 -0700

    Update spacing in the Y direction

--- src/components/Animation/Animation.js
+++ src/components/Animation/Animation.js
@@ -7,7 +7,7 @@ const Animation = () => {
   for (var i = 0; i < 150; i ++) {
     snowflake.push(Math.floor(Math.random() * 1000))
   }
-  const flakes = snowflake.map(flake => <Snowflake locationX={flake - 50} locationY={Math.floor(Math.random() * 1500)} key={Math.random()}/>);
+  const flakes = snowflake.map(flake => <Snowflake locationX={flake - 50} locationY={Math.floor(Math.random() * 1500) - 100} key={Math.random()}/>);
   return (
     <div>
       {flakes}
