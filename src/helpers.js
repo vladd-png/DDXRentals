c0fe03de73f65e172f5b2605d5be48f970e4f222
Virginia Ladd <virginialadd2@gmail.com>
2020-02-11 16:01:36 -0700

    Broke fetch call out of helper funciton

--- src/helpers.js
+++ src/helpers.js
@@ -1,9 +1,14 @@
-export const fetchRentalAreaData = areaData => {
+import { fetchAreaListings } from './apiCalls.js'
 
+export const fetchRentalAreaData = areaData => {
   const result = areaData.map(area => {
+    // fetchAreaListings(area.details)
     return fetch('http://localhost:3001' + area.details)
-      .then(response => response.json())
-      .then(data => data)
+     .then(response => response.json())
+      .then(data => {
+        console.log(data)
+        return data
+      })
       .catch(error => window.alert(`There was an error: ${error}`))
   })
   return result;
