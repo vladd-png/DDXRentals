037df7aea5a299e2c7daf05b36938b9b1d0190c7
Virginia Ladd <virginialadd2@gmail.com>
2020-02-11 14:34:17 -0700

    Create two functions on apiCalls to export fetch

---
+++ src/apiCalls.js
@@ -0,0 +1,19 @@
+export const getListings = () => {
+  return fetch('http://localhost:3001/api/v1/areas')
+    .then(response => {
+      if(!response.ok) {
+        throw Error('Error while fetching.')
+      }
+      return response.json()
+    })
+}
+
+export const updateListingState = () => {
+  return fetch('http://localhost:3001/api/v1/listings')
+    .then(response => {
+      if(!response.ok) {
+        throw Error('Error while updating.')
+      }
+      return response.json()
+    })
+}
