8f99d80272ccb69e8593db5389c4ea1a8acb764a
Zachary Nemeroff <zanemeroff@gmail.com>
2020-02-10 19:11:09 -0700

    Add nav tests

--- src/components/Nav/Nav.test.js
+++ src/components/Nav/Nav.test.js
@@ -5,23 +5,16 @@ import { shallow } from 'enzyme';
 
 describe('Nav', () => {
 
-  it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<Nav
-      {{userData={
-        check: false,
-        email: '',
-        name: '',
-        validateRouterLink: '',
-        value: "business"
-      }}}
-      />, div);
-    ReactDOM.unmountComponentAtNode(div);
-  });
-
   it('should be an instance of Nav component', () => {
+    const mockUserData = {
+      check: false,
+      email: '',
+      name: '',
+      validateRouterLink: '',
+      value: "business"
+    }
     const wrapper = shallow(<Nav
-
+      userData={mockUserData}
       />);
       expect(wrapper).toMatchSnapshot();
   });
