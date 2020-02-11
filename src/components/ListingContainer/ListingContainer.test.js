9c1d48e761abac1f16fb57428e3126b940300b4b
Virginia Ladd <virginialadd2@gmail.com>
2020-02-10 17:31:25 -0700

    Create and pass tests for ListingContainer component

--- src/components/ListingContainer/ListingContainer.test.js
+++ src/components/ListingContainer/ListingContainer.test.js
@@ -4,16 +4,30 @@ import ListingContainer from './ListingContainer';
 import { shallow } from 'enzyme';
 
 describe('ListingContainer', () => {
+  let wrapper, mockAddFavorite, mockArea;
 
-  it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<ListingContainer />, div);
-    ReactDOM.unmountComponentAtNode(div);
-  });
+  beforeEach(() => {
+    mockArea =  [{
+      listing_id: 3, area_id: 590, name: 'Hip RiNo Party Spot',
+      address: {
+        street: '2250 Lawrence St',
+        zip: '80205'
+      },
+      details: {
+        neighborhood_id: 5124122, superhost: true, seller_source: '91jss1', beds: 3, baths: 2.5, cost_per_night: 420, features: [
+          'hot tub',
+          'espresso machine'
+        ],
+      },
+      dev_id: 'u4gh2j', area: 'rino', db_connect: 834470
+    }]
+  })
 
   it('should be an instance of ListingContainer component', () => {
-    const wrapper = shallow(<ListingContainer
-
+    mockAddFavorite = jest.fn()
+    wrapper = shallow(<ListingContainer
+        addFavorite={mockAddFavorite}
+        listings={mockArea}
       />);
       expect(wrapper).toMatchSnapshot();
   });
