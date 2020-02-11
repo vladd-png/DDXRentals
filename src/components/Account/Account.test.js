673333f5f88a7eaacb9d18f89847f9f93f1a6813
Virginia Ladd <virginialadd2@gmail.com>
2020-02-10 17:18:50 -0700

    Create and pass snowflake test

--- src/components/Account/Account.test.js
+++ src/components/Account/Account.test.js
@@ -4,16 +4,24 @@ import Account from './Account';
 import { shallow } from 'enzyme';
 
 describe('Account', () => {
+  let mockUserInfo, mockFavorites;
 
-  it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<Account />, div);
-    ReactDOM.unmountComponentAtNode(div);
+  beforeEach(() => {
+    mockUserInfo = { email: 'ipitythefool@aol.com', name: 'Mr. T', value: 'Business' };
+    mockFavorites = {
+      about: "RiNo is a burgeoning area with new bars, restaurants and event spaces popping up all the time. Explore this thriving area of Denver today!",
+      id: 590,
+      listings: ["/api/v1/listings/3", "/api/v1/listings/44", "/api…"],
+      location: "North of Downtown Denver",
+      name: "River North",
+      nickName: "RiNo",
+    }
   });
 
   it('should be an instance of Account component', () => {
     const wrapper = shallow(<Account
-        
+        userData = {mockUserInfo}
+        favorites = {[mockFavorites]}
       />);
       expect(wrapper).toMatchSnapshot();
   });
